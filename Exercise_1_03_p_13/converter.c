7babed42665fb1a0bf3eb7458aedaf117e1a595b
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2020-12-31 12:25:47 -0300

    Small aesthetic changes and review.

--- Exercise_1_03_p_13/converter.c
+++ Exercise_1_03_p_13/converter.c
@@ -3,24 +3,27 @@
 
 int main()
 {
-
     const double lower = 0.0;
     const double upper = 300.0;
     const double step = 20.0;
     double fahr, celsius;
 
+    // header
     fahr = lower;
     printf("\n\tFAHR CELSIUS\n");
     printf("\t------------\n");
+
+    // program loop
     while (fahr <= upper)
     {
         celsius = (5.0/9.0) * (fahr - 32.0);
         printf("\t%4.0lf %6.1lf\n", fahr, celsius);
         fahr += step;
     }
+
+    // footer
     printf("\t------------\n\n");
 
     return EXIT_SUCCESS;
-
 }
 
