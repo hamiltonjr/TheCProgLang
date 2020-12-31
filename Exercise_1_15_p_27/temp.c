bf13233923d3d1c1d56b60e92a74fbd17e65460a
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2020-12-31 19:24:12 -0300

    Aesthetic changes.

--- Exercise_1_15_p_27/temp.c
+++ Exercise_1_15_p_27/temp.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define LOWER   0.0
 #define UPPER 300.0
 #define STEP   20.0
 
 /*
  * Print Fahrenheit-Celsius table
- * for fahr = 0, 20, ..., 300
+ * for fahr = 0, 20, ..., 300.
+ * But now using functions.
  */
 
 // prototpye
@@ -13,22 +15,23 @@ double ftoc(double fahr);
 
 int main()
 {
-
     double fahr, celsius;
     double lower, upper, step;
 
+    // header
     printf("\t--------------\n");
     printf("\tTEMP CONVERTER\n");
     printf("\t   ºF     ºC  \n");
     printf("\t--------------\n");
+
+    // program loop
     for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
     {
         printf("\t%6.2lf\t%6.2lf\n", fahr, ftoc(fahr));
     }
     printf("\t--------------\n");
 
-    return (0);
-
+    return EXIT_SUCCESS;
 }
  
 // This function converts temperatues from fahrenheit degrees 
@@ -37,4 +40,3 @@ double ftoc(double fahr)
 {
     return (5.0/9.0)*(fahr - 32.0);
 }
-
