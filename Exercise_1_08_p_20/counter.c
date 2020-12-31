8597f806f4ca9456ac108ea2bf0e478bbf4edd18
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2020-12-31 16:23:35 -0300

    Included counter (exercise 1.8).

---
+++ Exercise_1_08_p_20/counter.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nl = 0; // <enter> are counted
+    int nw = 1; // between n words has n-1 spaces
+    int nc = 1; // last <enter> is counted
+    int c;      // getchar() preference: int
+
+    // program loop
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ') { nc++; nw++; }
+        else if (c == '\n') nl++;
+        else nc++;
+    }
+
+    // output
+    printf("%d %d %d\n", nl, nw, nc);
+    return EXIT_SUCCESS;
+}
