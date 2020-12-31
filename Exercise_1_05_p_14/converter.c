243089048db0c4042380ebc30d3b79ecdd1c8408
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2020-12-31 15:23:04 -0300

    Inclusion of new conversion table (exercise 1.5).

---
+++ Exercise_1_05_p_14/converter.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// conversion table using for and inverted order
+int main()
+{
+    const double lower = 0.0;
+    const double upper = 300.0;
+    const double step = 20.0;
+    double fahr, celsius;
+
+    // header
+    fahr = lower;
+    printf("\n\tFAHR CELSIUS\n");
+    printf("\t------------\n");
+
+    // program loop
+    for (fahr = upper; fahr >= lower; fahr -= step)
+    {
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("\t%4.0lf %6.1lf\n", fahr, celsius);
+    }
+
+    // footer
+    printf("\t------------\n\n");
+
+    return EXIT_SUCCESS;
+}
