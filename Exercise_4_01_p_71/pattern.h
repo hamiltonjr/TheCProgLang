d718ec679b5fe8c7f5148857af96a921a15f72ac
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2021-01-05 15:54:59 -0300

    Shared files of XGrep projecy exercise 4.01.

---
+++ Exercise_4_01_p_71/pattern.h
@@ -0,0 +1,12 @@
+#ifndef PATTERN_H
+#define PATTERN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXLINE 1000
+
+int getline(char*, int);
+int strindex(char*, char*);
+int strrindex(char*, char*);
+
+#endif // PATTERN_H
