b2fa18ed52775d2d4f81c38f85a82ea626a4e289
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2021-01-13 08:08:02 -0300

    Exercise 2.01: some aesthetic changes.

--- Exercise_2_01_p_36/limits.c
+++ Exercise_2_01_p_36/limits.c
@@ -4,35 +4,23 @@
 
 int main()
 {
+    printf("\n\tMinimum and maximum values a `signed char' can hold.\n");
+    printf("\tSCHAR_MIN = %d\n", SCHAR_MIN);
+    printf("\tSCHAR_MAX = %d\n", SCHAR_MAX);
 
-    printf("Minimum and maximum values a `signed char' can hold.\n");
-    printf("SCHAR_MIN = %d\n", SCHAR_MIN);
-    printf("SCHAR_MAX = %d\n", SCHAR_MAX);
+    printf("\tMaximum value an `unsigned char' can hold.\n");
+    printf("\tUCHAR_MAX = %d\n", UCHAR_MAX);
 
-    printf("Maximum value an `unsigned char' can hold.\n");
-    printf("UCHAR_MAX = %d\n", UCHAR_MAX);
+    printf("\tMinimum and maximum values a `char' can hold.\n");
+    printf("\tIn this architeture the char unsigned is definde.\n");
+    printf("\tCHAR_MIN = %d\n", CHAR_MIN);
+    printf("\tCHAR_MAX = UCHAR_MAX = %d\n",  UCHAR_MAX);
+    printf("\tCHAR_MIN = SCHAR_MIN = %d\n", SCHAR_MIN);
+    printf("\tCHAR_MAX = SCGAR_MAX = %d\n", SCHAR_MAX);
 
-    printf("Minimum and maximum values a `char' can hold.\n");
-    printf("In this architeture the char unsigned is definde.\n");
-    printf("CHAR_MIN = %d\n", CHAR_MIN);
-    printf("CHAR_MAX = UCHAR_MAX = %d\n",  UCHAR_MAX);
-    printf("CHAR_MIN = SCHAR_MIN = %d\n", SCHAR_MIN);
-    printf("CHAR_MAX = SCGAR_MAX = %d\n", SCHAR_MAX);
-
-    printf("Minimum and maximum values a `signed short int' can hold.\n");    printf("SHRT_MIN = %d\n", SHRT_MIN);
-    printf("SHRT_MAX = %d\n", SHRT_MAX);
-
-
-
-
-
-
-
-
-
-    
+    printf("\tMinimum and maximum values a `signed short int' can hold.\n");
+    printf("\tSHRT_MIN = %d\n", SHRT_MIN);
+    printf("\tSHRT_MAX = %d\n\n", SHRT_MAX);
 
     return EXIT_SUCCESS;
-
 }
-
