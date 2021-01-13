ad3e2eb19722933ed1ac413c37d2848244a6eb55
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2021-01-13 06:06:27 -0300

    Changes in exercise 1.15: inclusion of a makefile and adapting for use of utils.a library.

--- Exercise_1_15_p_27/temp.c
+++ Exercise_1_15_p_27/temp.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../lib/utils.h"
 #define LOWER   0.0
 #define UPPER 300.0
 #define STEP   20.0
 
 /*
- * Print Fahrenheit-Celsius table
- * for fahr = 0, 20, ..., 300.
+ * Print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300.
  * But now using functions.
+ * OBS: fahr_to_celsius() is in utils.a library.
  */
-
-// prototpye
-double ftoc(double fahr);
-
 int main()
 {
-    double fahr, celsius;
-    double lower, upper, step;
+    double fahr;
 
     // header
     printf("\t--------------\n");
@@ -27,16 +23,10 @@ int main()
     // program loop
     for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
     {
-        printf("\t%6.2lf\t%6.2lf\n", fahr, ftoc(fahr));
+        printf("\t%6.2lf\t%6.2lf\n", fahr, 
+            fahr_to_celsius(fahr));
     }
     printf("\t--------------\n");
 
     return EXIT_SUCCESS;
-}
- 
-// This function converts temperatues from fahrenheit degrees 
-// to celsius degrees.
-double ftoc(double fahr)
-{
-    return (5.0/9.0)*(fahr - 32.0);
 }
