bfedf07f9652dd8b94b47bad7cddc7421d253af7
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2021-01-13 03:29:08 -0300

    Implementation of new utilities functions for utils.a library.

---
+++ lib/src/celsius_to_fahr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * Temperature conversion: Celsius degree to 
+ * Fahrenheit degree.
+ */
+
+double celsius_to_fahr(double celsius)
+{
+    return (9.0/5.0) * celsius + 32.0;
+}
