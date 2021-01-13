a8f51ae181d71d73305441d22d4858e00992146c
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2021-01-13 07:42:13 -0300

    Changes for use of utils.a library in exercise 1.17.

--- Exercise_1_17_p_31/main.c
+++ Exercise_1_17_p_31/main.c
@@ -1,23 +1,23 @@
-#include "gt80.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lib/utils.h"
+#define MAXLINE 1024
 #define MAX 80
 
 int main()
 {
-
     char line[MAXLINE];
     int len = 0;
 
     // catching lines and searching longest
+    printf("\n");
     while ((len = getline(line, MAXLINE)) > 0)
     { 
         if (len > MAX)
         {
             printf("[%03d] %s\n", len, line);
         }
-
     }
 
     return EXIT_SUCCESS;
-
 }
-
