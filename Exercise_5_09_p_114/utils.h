4859b81e0198c35889e98ea6a7e06700bf3f83aa
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2021-01-15 09:05:17 -0300

    Exercise 5.09: review and changes for simplify code.

--- Exercise_5_09_p_114/utils.h
+++ Exercise_5_09_p_114/utils.h
@@ -1,12 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-
-#define MAX
 
 // prototype(s)
 int day_of_year(int, int, int);
